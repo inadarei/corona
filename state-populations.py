c84e57d746caf9c1c8e9174379de18ec164685ae
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2020-04-05 05:41:58 +0000

    populations.js generator script cleaned up

--- state-populations.py
+++ state-populations.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 # https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/state/detail/
 import pandas as pd
 import json
@@ -7,11 +9,11 @@ state_data = pd.read_csv("./census.csv")
 zz = state_data \
    .filter(items=['NAME', 'POPESTIMATE2019']);
 
-print(zz)
-
 data = state_data.set_index('NAME') \
    .filter(items=['NAME', 'POPESTIMATE2019']) \
    .to_dict()
 
-with open('populations.json', 'w') as f:
-    json.dump(data['POPESTIMATE2019'], f)
+data_js = "const state_populations = " + json.dumps(data['POPESTIMATE2019'])
+
+with open('populations.js', 'w') as f:
+    f.write(data_js)    
