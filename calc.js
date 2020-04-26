7842c3a6e60a8d8ef6a0b6399ff84206818cfc98
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2020-04-26 15:44:19 +0000

    added weighted moving averages

--- calc.js
+++ calc.js
@@ -26,30 +26,67 @@ function percapita(data, population) {
   return data_pop;
 }
 
-function doubling_nums(data) {
+function growth_rates(data) {
   let ret_data = {}
-  let past_val, growth_rate, doubling_time;
+  let past_val, growth_rate;
   for (var key in data) {
-    if (key < "2020-03-22") continue;
-    console.log(key);
     if (data.hasOwnProperty(key)) {
-      if (past_val === 0) {
-        growth_rate = Number.MAX_SAFE_INTEGER;
+      
+      if (key < "2020-03-22") { 
+        past_val = data[key];
+        continue;
+      }
+
+      if (!past_val || typeof past_val === "undefined" || past_val == 0) {
+        past_val = Number.MAX_SAFE_INTEGER;
+        growth_rate = 0;
       } else {
         growth_rate = Math.round((data[key] - past_val)/past_val * 100);
       }
+
       past_val = data[key];
-      doubling_time = growth_rate;
-      ret_data[key] = doubling_time;
-    }
+      ret_data[key] = growth_rate;
+    }  
   }
   return ret_data;
+}
+
+function weighted_moving_averages(_data) {
+  console.log("wma");
+  console.dir(_data);
+
+  const numvalues = Object.values(_data);
+  const keys = Object.keys(_data);
+  const result = {};
+  let counter = 0;
+  const wma_cap = 14; // max points in the past to take into account, for weighted moving averages
+
+  for (const key of  keys) {
+    const currKey = keys[counter];
+    //let indexes = "";
+    let sum_of_weights = 0;
+    let sum_of_values = 0;
+    for (let i = counter; i>=0; i--) {
+      if ((counter - i) > wma_cap) break;
+      const n = i+1; // must start with 1
+      //indexes += n + " ";
+      sum_of_values += n * numvalues[i];
+      sum_of_weights += n;
+    }
+    result[key] = sum_of_values / sum_of_weights;
+    counter++;
+    //console.log(indexes)
+  }
+
+  console.log(result);
+  return result;
 }
 
 function draw (states, type) {
   let data = [];
   let data_percapita = [];
-  let data_doubling = [];
+  let data_growth = [];
+  let data_growth_wma = []; //weighted moving averages for smoothing
 
   states.forEach((state) => {
     const only_state = states_data[state][type];
@@ -84,20 +121,32 @@ function draw (states, type) {
     states.forEach((state) => {
       const only_state = states_data[state][type];
       
-      const plot_data = to_plot_data(doubling_nums(only_state))
+      const growth_nums = growth_rates(only_state);
+      const growth_nums_wma = weighted_moving_averages(growth_nums);
+      const plot_data = to_plot_data(growth_nums);
+      const plot_data_wma = to_plot_data(growth_nums_wma);
+
       plot_data["name"] = state;
       //plot_data['mode'] = 'markers';
       plot_data['type'] = 'scatter';
-    
-      data_doubling.push(plot_data);
+      data_growth.push(plot_data);
+
+      plot_data_wma["name"] = state;
+      //plot_data['mode'] = 'markers';
+      plot_data_wma['type'] = 'scatter';
+      data_growth_wma.push(plot_data_wma);
     })
   
     layout = {
-      //title: ' Doubling Time of ' + capitalize(type) + ', in Days'
-      title: ' Growth rate of ' + capitalize(type)
+      title: ' Growth rate of ' + capitalize(type) + ' (Actuals)'
     };  
-    Plotly.newPlot(type + "_doubling", data_doubling, layout);
-  
+    Plotly.newPlot(type + "_growth", data_growth, layout);
+
+    layout = {
+      title: ' Growth rate of ' + capitalize(type) + ' (Weighted Moving Averages)'
+    };  
+    Plotly.newPlot(type + "_growth_wma", data_growth_wma, layout);
+
   }
 
 
